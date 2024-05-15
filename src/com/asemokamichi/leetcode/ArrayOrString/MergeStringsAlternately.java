9b271c5924cdfef06b66039ec6b25b1871ea2271
Asemokamichi <asemokamichi@gmail.com>
2024-05-15 22:36:27 +0500

    solving the 'Merge Strings Alternately '

--- src/com/asemokamichi/leetcode/ArrayOrString/MergeStringsAlternately.java
+++ src/com/asemokamichi/leetcode/ArrayOrString/MergeStringsAlternately.java
@@ -1,5 +1,15 @@
 package com.asemokamichi.leetcode.ArrayOrString;
 
-public class MergeStringsAlternately {
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < word1.length() && i < word2.length(); i++) {
+            s.append(word1.charAt(i)).append(word2.charAt(i));
+        }
 
+        return s.append(word1.length() > word2.length() ?
+                word1.substring(word2.length()) :
+                word2.substring(word1.length())
+        ).toString();
+    }
 }
