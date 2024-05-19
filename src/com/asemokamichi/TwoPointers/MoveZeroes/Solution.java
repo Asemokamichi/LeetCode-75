9400c272863d32c31bd734f486882b26f6fe54a8
Asemokamichi <asemokamichi@gmail.com>
2024-05-19 22:12:27 +0500

    adding solving new tasks

---
+++ src/com/asemokamichi/TwoPointers/MoveZeroes/Solution.java
@@ -0,0 +1,15 @@
+package com.asemokamichi.TwoPointers.MoveZeroes;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        var index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]!=0){
+                var tmp = nums[i];
+                nums[i]=nums[index];
+                nums[index] = tmp;
+                index++;
+            }
+        }
+    }
+}
