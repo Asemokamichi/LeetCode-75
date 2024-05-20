203422186d8a13c4c18e5497c04337dd25e27ad8
Asemokamichi <asemokamichi@gmail.com>
2024-05-21 00:02:36 +0500

    adding solving new tasks

---
+++ src/com/asemokamichi/leetcode/DifferentTasks/ZigzagConversion/Solution.java
@@ -0,0 +1,37 @@
+package com.asemokamichi.leetcode.DifferentTasks.ZigzagConversion;
+
+class Solution {
+    public static String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+        var num = Math.ceil(s.length() * 2.0 / (numRows * 2 - 2));
+        char[][] array = new char[(int)num][numRows];
+        for (int i = 0, q = 0; i < array.length; i++) {
+            if (i % 2 != 0) {
+                for (int j = array[i].length - 2; j > 0; j--) {
+                    if (q == s.length()) break;
+                    array[i][j] = s.charAt(q++);
+                }
+            } else {
+                for (int j = 0; j < array[i].length; j++) {
+                    if (q == s.length()) break;
+                    array[i][j] = s.charAt(q++);
+                }
+            }
+        }
+
+        StringBuilder str = new StringBuilder();
+        for (int j = 0; j < array[0].length; j++) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i][j] != 0) str.append(array[i][j]);
+            }
+        }
+        return str.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        int numRows = 4;
+        System.out.println(convert(s, numRows));
+    }
+}
