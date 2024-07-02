c9a4702a97479f8c9ff3759a82546b2c80ffa30d
Asemokamichi <asemokamichi@gmail.com>
2024-07-02 21:27:29 +0500

    added new project

--- src/com/asemokamichi/leetcode/DifferentTasks/IntersectionOfTwoArraysII/Solution.java
+++ src/com/asemokamichi/leetcode/DifferentTasks/IntersectionOfTwoArraysII/Solution.java
@@ -9,22 +9,14 @@ public class Solution {
         int i = 0, j = 0, k = 0;
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        while( i < l1 && j < l2)
-        {
-            if(nums1[i] < nums2[j])
-            {
-                i++;
-            }
-            else if(nums1[i] > nums2[j])
-            {
-                j++;
-            }
-            else
-            {
+        while (i < l1 && j < l2) {
+            if (nums1[i] < nums2[j]) i++;
+            else if (nums1[i] > nums2[j]) j++;
+            else {
                 nums1[k++] = nums1[i++];
                 j++;
             }
         }
-        return Arrays.copyOfRange(nums1,0,k);
+        return Arrays.copyOfRange(nums1, 0, k);
     }
 }
